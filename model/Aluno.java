46c9f1dcb0a8b13a672dd01ed61f9efa02f4f1d0
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-05-05 21:48:16 -0300

    Add files via upload

---
+++ model/Aluno.java
@@ -0,0 +1,39 @@
+package poo.model;
+
+public class Aluno {
+	
+	
+	private String Nome;
+	private String Cpf;
+	private String Email;
+	private String Contato;
+	
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		this.Nome = nome;
+	}
+	public String getCPF() {
+		return Cpf;
+	}
+	public void setCPF(String cpf) {
+		Cpf = cpf;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getContato() {
+		return Contato;
+	}
+	public void setContato(String contato) {
+		Contato = contato;
+	}
+	
+	
+
+}
---
+++ model/Professor.java
@@ -0,0 +1,30 @@
+package poo.model;
+
+public class Professor {
+	
+	private String Nome;
+	private String Matricula;
+	private String Email;
+
+
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		this.Nome = nome;
+	}
+	public String getMaticula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+
+}
